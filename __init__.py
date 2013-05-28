e1192eaf87383f1220ba03ed3bda9f24e9757eef
shyeon.kim <shyeon.kim@scipi.net>
2013-05-28 16:06:31 +0900

    Adding __init__
    Signed-off-by: shyeon.kim <shyeon.kim@scipi.net>

---
+++ __init__.py
@@ -0,0 +1,13 @@
+# python-markovchain
+# Copyright 2013 Seonggyun Kim
+# See LICENSE for details.
+
+"""
+Python Markov chain library
+"""
+__version__ = '0.12'
+__author__ = 'Seonghyun Kim'
+__license__ = 'MIT'
+
+from python-markovchain.markovchains import MarkovChains
+
